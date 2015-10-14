9b2cfbb4d97346ecec6c1461a67776722cf25df1
Arturo Rinaldi <arty.net2@gmail.com>
2015-10-14 16:58:46 +0200

    addign armhf bash script

---
+++ arch.armhf.build.bash
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+export HIDAPI_LDFLAGS="-lhidapi-libusb"
+
+./clean.bash
+rm -rf objdir
+
+./libusb.build.bash
+./libusb-compat-0.1.build.bash
+USE_LOCAL_LIBUSB=yes ./hidapi.build.bash
+USE_LOCAL_LIBUSB=yes ./openocd.build.bash
+
+if [[ -f objdir/bin/openocd ]] ;
+then
+	strip --strip-all objdir/bin/openocd
+	mv objdir/bin/openocd objdir/bin/openocd.bin
+	cp launchers/openocd.linux objdir/bin/openocd
+	chmod +x objdir/bin/openocd
+fi
+
+ARCH=`gcc -v 2>&1 | awk '/Target/ { print $2 }'`
+
+rm -rf OpenOCD-0.9.0-arduino
+rm -f OpenOCD-0.9.0-arduino.org-$ARCH.tar.bz2
+mv objdir OpenOCD-0.9.0-arduino
+tar cfvj OpenOCD-0.9.0-arduino.org-$ARCH.tar.bz2 OpenOCD-0.9.0-arduino
+
