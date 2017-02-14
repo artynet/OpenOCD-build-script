8496dc17ab66c494fa497d4ef49d64c7c4087000
Arturo Rinaldi <arty.net2@gmail.com>
2017-02-14 13:35:10 +0100

    adding script to clean working dirs

---
+++ clean_all_openocd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+list="libusb-1.0.20 libusb-compat-0.1.5 eudev-3.1.5 hidapi OpenOCD OpenOCD-0.10.0 libusb-compat-0.1"
+
+for i in $list
+do
+    cd $i
+    make distclean
+    cd ..
+done
+
+# reset hidapi
+git checkout hidapi
+
+# reset libusb-compat-0.1 trunk
+cd libusb-compat-0.1/
+git reset --hard HEAD
+cd ..
+
+# removing useless files
+git clean -f -d
