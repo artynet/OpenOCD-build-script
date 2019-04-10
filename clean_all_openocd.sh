efd41e978ff080b490a7ed3b92bb60e1b455ee24
Arturo Rinaldi <arty.net2@gmail.com>
2019-04-10 02:12:13 +0200

    improving cleaning after single build

--- clean_all_openocd.sh
+++ clean_all_openocd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-list="libusb-1.0.22 libusb-compat-0.1.5 eudev-3.2.7 hidapi OpenOCD libusb-compat-0.1"
+list="libusb-1.0.22 libusb-compat-0.1.7 eudev-3.2.7 hidapi OpenOCD libusb-compat-0.1"
 
 for i in $list
 do
@@ -12,11 +12,22 @@ done
 # reset hidapi
 git checkout hidapi
 
-# reset libusb-compat-0.1.5 module
-git checkout libusb-compat-0.1.5
+# reset libusb-compat-0.1.7 module
+git checkout libusb-compat-0.1.7
+
+# reset eudev-3.2.7 module
+git checkout eudev-3.2.7
 
 # reset libusb-compat-0.1 trunk
 cd libusb-compat-0.1/
+git reset --hard HEAD
+cd ..
+
+# removing autom4te cache where needed
+rm -rf libusb*/autom4te.cache
+
+# reset OpenOCD trunk
+cd OpenOCD/
 git reset --hard HEAD
 cd ..
 
